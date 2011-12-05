414fe9a3c6bf5884bfc199e764bc752ec790f3d0
JeanVEGA <ondrusek.jan@gmail.com>
2011-12-05 20:22:37 +0100

    modified makefiles to include relative path, tested with valgrind for zero leanks

--- Debug/src/logging/subdir.mk
+++ Debug/src/logging/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 src/logging/%.o: ../src/logging/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cygwin C++ Compiler'
-	g++ -I"D:\Documents\Development\workspaceSchool\MI-DPO-SmartPointer\src" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I"./src" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Release/sources.mk
+++ Release/sources.mk
@@ -24,7 +24,5 @@ C_UPPER_DEPS :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 src/smart_pointer \
-src/naive_impl \
 src \
 src/logging \
-
